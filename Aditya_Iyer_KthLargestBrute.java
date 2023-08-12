837280bf3f3cae80693927ef1eb59ee87dcaa085
Adi3005 <134708942+Adi3005@users.noreply.github.com>
2023-08-12 15:10:47 +0530

    Create Aditya_Iyer_KthLargestBrute.java

---
+++ Aditya_Iyer_KthLargestBrute.java
@@ -0,0 +1,33 @@
+/*Aditya K Iyer*/
+/*Was asked to find the 2nd largest value in the array*/
+import java.util.*;
+
+class kthLargest{
+
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {14,7,19,81,22,34,54,97};
+        int i=0;
+	int max = Integer.MIN_VALUE;
+	int maxid = 0;
+	for(i=0;i<n;i++)
+	if(arr[i]>max){
+		max = arr[i];
+		maxid = i;
+	}
+	arr[maxid] = Integer.MIN_VALUE;
+        
+	max = Integer.MIN_VALUE;
+	
+	for(i=0;i<n;i++)
+	if(arr[i]>max)
+		max = arr[i];
+	
+	
+
+                
+        System.out.println(quickSelect(max);    
+        
+        }
+    }
